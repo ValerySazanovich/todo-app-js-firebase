db3e782758c8d2d5d6fb9a36a692a7f1ed28a8fb
Valery Sazanovich <valerysazanovich@gmail.com>
2025-12-12 17:27:36 +0300

    add killer features

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ import { toggleTodoStatus } from "./API/getStatusTodoApi.js";
 import { deleteTodo } from "./API/deleteTodoApi.js";
 import { updateTodo } from "./API/updateTodoApi.js";
 import { addTodo } from "./API/addTodoApi.js";
+import { updateTaskOrderOnServer } from "./API/updateTasksOrderApi.js";
+import { deleteCompletedTodos } from "./API/deleteCompletedTodosApi.js";
 
 const container = document.getElementById("posts-container");
 const taskInput = document.getElementById("task-input");
 const addButton = document.getElementById("add-button");
 const downloadButton = document.querySelector(".button-download");
 const overlay = document.getElementById("overlay");
+const deleteCompletedButton = document.getElementById(
+  "delete-completed-button"
+);
 
 export const host = "https://68a43188c123272fb9b1b38d.mockapi.io/api/v1/todos";
 
@@ -16,9 +21,15 @@ async function loadData() {
   try {
     showLoader();
     const todos = await getTodos();
+
     renderData(todos);
   } catch (error) {
     console.error(error.message);
+    if (error.message === "Задач нет") {
+      showError("Задач нет");
+    } else {
+      showError("Не удалось получить данные");
+    }
   } finally {
     hideLoader();
   }
@@ -26,9 +37,15 @@ async function loadData() {
 
 function renderData(todos) {
   container.innerHTML = "";
+
+  const hasCompletedTodos = todos.some((todo) => todo.completed);
+
+  deleteCompletedButton.style.display = hasCompletedTodos ? "block" : "none";
+
   todos.forEach((todo) => {
     const todoElement = document.createElement("div");
     todoElement.classList.add("todo");
+    todoElement.setAttribute("data-id", todo.id);
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
@@ -40,6 +57,7 @@ function renderData(todos) {
         await loadData();
       } catch (error) {
         console.error(error.message);
+        showError("Не удалось изменить статус задачи");
       }
     });
 
@@ -72,6 +90,7 @@ function renderData(todos) {
         await loadData();
       } catch (error) {
         console.error(error.message);
+        showError("Не удалось удалить задачу");
       }
     });
 
@@ -86,13 +105,27 @@ function renderData(todos) {
     updateButton.append(updateIcon);
 
     updateButton.addEventListener("click", async () => {
-      const newText = prompt("Введите новый текст задачи:", todo.text);
+      const { value: newText } = await Swal.fire({
+        title: "Редактирование задачи",
+        input: "text",
+        inputLabel: "Введите текст новой задачи",
+        inputValue: todo.text,
+        showCancelButton: true,
+        confirmButtonText: "Сохранить",
+        cancelButtonText: "Отмена",
+        inputValidator: (value) => {
+          if (!value) {
+            return "Поле не может быть пустым!";
+          }
+        },
+      });
+
       if (newText) {
         try {
           await updateTodo(todo.id, newText);
           await loadData();
         } catch (error) {
-          console.error(error.message);
+          showError("Не удалось обновить задачу");
         }
       }
     });
@@ -105,6 +138,7 @@ function renderData(todos) {
       updateButton
     );
 
+    addDragAndDropListeners(todoElement, todo);
     container.append(todoElement);
     downloadButton.hidden = true;
     hideLoader();
@@ -131,7 +165,8 @@ async function addNewTodo() {
     taskInput.value = "";
     await loadData();
   } catch (error) {
-    console.error(`Ошибка добавления:`, error.message);
+    console.error(error.message);
+    showError("Не удалось добавить задачу");
   }
 }
 
@@ -145,10 +180,106 @@ taskInput.addEventListener("keydown", (event) => {
 
 downloadButton.addEventListener("click", loadData);
 
+deleteCompletedButton.addEventListener("click", async () => {
+  const { isConfirmed } = await Swal.fire({
+    title: "Вы уверены?",
+    text: "Все выполенные задачи будут удалены!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Да, удалить!",
+    cancelButtonText: "Отменить",
+  });
+
+  console.log(isConfirmed);
+
+  if (!isConfirmed) {
+    return;
+  }
+
+  try {
+    await deleteCompletedTodos(container);
+    await loadData();
+  } catch (error) {
+    console.error(error.message);
+    showError("Не удалось удалить список задач");
+  }
+});
+
 function showLoader() {
   overlay.style.display = "flex";
 }
 
 function hideLoader() {
   overlay.style.display = "none";
+}
+
+function addDragAndDropListeners(todoElement, todo) {
+  todoElement.draggable = true;
+  todoElement.addEventListener("dragstart", (event) => {
+    event.dataTransfer.setData("text/plain", todo.id);
+    event.currentTarget.classList.add("dragging");
+  });
+
+  todoElement.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    const overElement = event.currentTarget;
+
+    if (overElement !== draggable) {
+      const rect = overElement.getBoundingClientRect();
+      const offset = event.clientY - rect.top;
+      if (offset < rect.height / 2) {
+        container.insertBefore(draggable, overElement);
+      } else {
+        container.insertBefore(draggable, overElement.nextSibling);
+      }
+    }
+  });
+
+  todoElement.addEventListener("dragend", (event) => {
+    event.currentTarget.classList.remove("dragging");
+
+    updateTaskOrder();
+  });
+}
+
+async function updateTaskOrder() {
+  const todos = [...container.querySelectorAll(".todo")];
+  const updatedOrder = todos.map((todo, index) => {
+    return {
+      id: todo.getAttribute("data-id"),
+      order: index + 1,
+    };
+  });
+
+  try {
+    showLoader();
+    for (const task of updatedOrder) {
+      await updateTaskOrderOnServer(task.id, task.order);
+    }
+
+    console.log("Порядок задач обновлен");
+
+    return true;
+  } catch (error) {
+    console.error(error.message);
+    showError("Не удалось переместить элемент");
+  } finally {
+    hideLoader();
+  }
+}
+
+function showError(message) {
+  const icon = message === "Задач нет" ? "info" : "error";
+  const title = message === "Задач нет" ? "Информация" : "Ошибка";
+  const text = message === "Задач нет" ? "У Вас нет задач" : message;
+
+  Swal.fire({
+    title,
+    text,
+    icon,
+    showConfirmButton: true,
+  });
 }
